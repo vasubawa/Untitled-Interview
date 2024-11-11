f53322c0dcb338b7ef36d052fc72515d68e316a3
muzamil <mshamsifl@gmail.com>
2024-11-11 17:41:58 -0500

    setup file structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
+    </StrictMode>
 )
