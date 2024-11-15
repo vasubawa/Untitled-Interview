6113096dd08709e479e468c1e0cee6317f8e997b
muzamil <mshamsifl@gmail.com>
2024-11-14 21:48:05 -0500

    changed from TS to JS

--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createSystem, defaultTheme } from "@chakra-ui/react"
-export const system = createSystem(defaultTheme, {
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-  },
-});
-
-export default defaultTheme
