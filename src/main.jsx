eb278afec6366eb4008b0d1517f5fa3f516dde21
Muzamil Shamsi <127911986+muzamil-dev@users.noreply.github.com>
2024-12-05 21:07:37 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.js";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
