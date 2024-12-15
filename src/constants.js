815ec7e769ad0705c19740a3ab02a40b34ab3a0b
Muzamil Shamsi <127911986+muzamil-dev@users.noreply.github.com>
2024-12-14 21:14:30 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -7,6 +7,7 @@ export const LANGUAGE_VERSIONS = {
   php: "8.2.3",
 };
 
+
 export const CODE_SNIPPETS = {
   javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
